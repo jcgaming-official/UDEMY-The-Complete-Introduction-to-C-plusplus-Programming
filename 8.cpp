988c822374b8b01a7b20cae26368456738879f44
John Carl Quieta <77482435+jcgaming-official@users.noreply.github.com>
2023-10-05 20:54:52 +0000

    topic#8 done

---
+++ 8.cpp
@@ -0,0 +1,16 @@
+#include <iostream>;
+using namespace std;
+
+int main(){
+    double a = 43;
+    double b = 2.6;
+    double c = 54.82;
+    double d = 433.34;
+    double add = a-b;
+    double mul = b*c;
+    double all = a+b*d/c+(b+c)/b*(a+b+c)/c;
+
+    cout<<add << endl;
+    cout<<all<< endl;
+    return 0;
+}
