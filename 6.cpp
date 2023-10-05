a586995a892710b3b9c88d7f685dc5512f5fdfd4
John Carl Quieta <77482435+jcgaming-official@users.noreply.github.com>
2023-10-05 19:51:33 +0000

    topic 6 done

---
+++ 6.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main (){
+    string food1 = "Chicken";
+    string food2 = "Spaghetti";
+    string food3 = "Burger";
+
+    cout << food1 << food2 << food3 << endl;
+
+    cout <<"I ate " << food2 << endl;
+    cout << food3 <<" is great" << endl;
+    cout << food1 <<" is delicious and "<< food2 <<" is best to eat together with "<< food3 << endl;
+
+    return 0;
+}
