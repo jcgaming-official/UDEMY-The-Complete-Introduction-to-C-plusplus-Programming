8ba5b48923f236308abd9c375e1a9fe29562b150
John Carl Quieta <77482435+jcgaming-official@users.noreply.github.com>
2023-10-06 06:59:12 +0000

    topic#14 done

---
+++ 13.cpp
@@ -0,0 +1,16 @@
+//array
+#include<iostream>
+using namespace std;
+
+int main(){
+    string food []={"Pizza","Burger","Fries","Chocolate","Ice Cream"};
+    string drink [7]={};
+    drink[0]="Juice";
+    drink[3]="Water";
+    drink[5]="Beer";
+    cout<< food [3]<<endl;
+    cout<<drink[3]<<endl;
+    cout<<drink[0]<<endl;
+
+    return 0;
+}
